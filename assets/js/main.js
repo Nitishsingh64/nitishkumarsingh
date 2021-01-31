c60f636a787aa3fbd87eba5737e8029f9cc9efee
Nitishsingh64 <68182058+Nitishsingh64@users.noreply.github.com>
2021-01-31 16:36:35 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,11 @@
     
     "use strict";
     
-    //===== Prealoder
-    
     $(window).on('load', function(event) {     
         $('.preloader').delay(500).fadeOut(500);
     });
     
-    
-    
-    
-    //===== Sticky
-    
-    $(window).on('scroll', function(event) {     //when we scrool down from top some amount of top layes decreses 
+    $(window).on('scroll', function(event) {    
         var scroll = $(window).scrollTop();
         if (scroll < 10) {
             $(".navigation").removeClass("sticky");
